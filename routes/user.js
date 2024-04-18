94e6fbeaa318d93c4d1e1caddadd4429f905db52
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-04-18 20:54:57 +0530

    add send-email util

--- routes/user.js
+++ routes/user.js
@@ -3,28 +3,24 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const { sendOTPEmail } = require('../utils/email');
+const otpGenerator = require('otp-generator')
 
 
 router.post('/register', async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ error: 'User already exists' });
     }
 
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // Generate OTP and save it in the user document
-    const otp = Math.floor(100000 + Math.random() * 900000);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+    // const otp = Math.floor(100000 + Math.random() * 900000);
     const user = new User({ email, password: hashedPassword, otp });
     await user.save();
 
-    // Send the OTP to the user's email
     await sendOTPEmail(email, otp);
 
     res.status(201).json({ message: 'User registered successfully' });
---
+++ utils/email.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER_ID,
+    pass: process.env.PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+async function sendOTPEmail(email, otp) {
+  try {
+    await transporter.sendMail({
+      from: process.env.USER_ID,
+      to: email,
+      subject: "OTP for Account Verification",
+      html: `<p>OTP to verify your account is ${otp}</p>`,
+    });
+    console.log("OTP sent successfully");
+  } catch (error) {
+    console.error("Error sending OTP:", error);
+    throw new Error("Failed to send OTP");
+  }
+}
+
+module.exports = {sendOTPEmail}
