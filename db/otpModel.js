0535427f2a81dbacd0efeab25c667af152a87ce8
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-04-19 15:37:53 +0530

    add get user data using jwt

--- db/otpModel.js
+++ db/otpModel.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 
 const otpSchema = new mongoose.Schema({
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+    default: function () {
+      return new Date(Date.now() + 300 * 1000);
     },
-    otp: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    expiresAt: {
-      type: Date,
-      default: function() {
-        return new Date(Date.now() + 60 * 1000); 
-      },
-    },
-  });
+  },
+});
 
 module.exports = mongoose.model("OTP", otpSchema);
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,8 @@ const User = require("../db/UserModel");
 const OTP = require("../db/otpModel");
 const { sendOTPEmail } = require("../utils/email");
 const otpGenerator = require("otp-generator");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const { authenticate } = require("../middleware/auth");
 
 router.post("/register", async (req, res) => {
   try {
@@ -41,21 +42,22 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/verify-otp", async (req, res) => {
+router.post("/verify", async (req, res) => {
   try {
-    const { email, otp, location, age, work } = req.body;
+    const { email, otp } = req.body;
     if (!otp) {
       res.status(500).json({ error: "Please enter OTP" });
     }
-    if (!location || !age || !work) {
-      res.status(500).json({ error: "Please enter additional user detailss" });
-    }
+    // if (!location || !age || !work) {
+    //   res.status(500).json({ error: "Please enter additional user detailss" });
+    // }
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
 
     const otpDocument = await OTP.findOne({ userId: user._id, otp });
+
     if (!otpDocument) {
       return res.status(400).json({ error: "Invalid OTP" });
     }
@@ -81,36 +83,71 @@ router.post("/verify-otp", async (req, res) => {
         .status(400)
         .json({ error: "OTP expired, a new OTP has been sent to your email" });
     }
+
+    
+
+    
     if (user.is_verified) {
-      return res.status(200).json({ message: "User already verified" });
+      return res.status(200).json({ message: "User already verified." });
     }
 
     user.is_verified = true;
-    user.location = location;
-    user.age = age;
-    user.work = work;
+    // user.location = location;
+    // user.age = age;
+    // user.work = work;
     user.updatedAt = new Date();
     await user.save();
 
     await OTP.deleteOne({ _id: otpDocument._id });
 
-    res.status(200).json({ message: "User verified and updated" });
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1 hour",
+    });
+    return res
+      .status(200)
+      .json({ token: token, message: "User has been verified!" });
+
+    // res.status(200).json({ message: "User verified and updated" });
   } catch (error) {
+    console.log(error)
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
+router.post("/update", authenticate, async (req, res) => {
+  const { location, age, work } = req.body;
+  try {
+    const user = req.user;
+    if (!user.is_verified) {
+      return res.status(400).json({ error: "User is not verified" });
+    }
+    user.location = location;
+    user.age = age;
+    user.work = work;
+    user.updatedAt = new Date();
+
+    await user.save();
+    res.status(200).json({ message: "User data updated successfully" });
+  } catch (error) {
+    console.error("Error updating user data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
+    if (!user.is_verified) {
+      return res.status(401).json({ message: "User is not verified" });
+    }
+
     const passwordMatch = await bcrypt.compare(password, user.password);
-  
     if (!passwordMatch) {
       return res.status(401).json({ message: "Incorrect password" });
     }
@@ -119,8 +156,22 @@ router.post("/login", async (req, res) => {
     });
     res.json({ token });
   } catch (error) {
-    // console.error("Error:", error);
+    console.log(error);
     res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/data", authenticate, async (req, res) => {
+  try {
+    const user = req.user;
+    if(!user.is_verified){
+      return res.status(400).json({ error: "User is not verified" });
+    }
+    const { email, location, age, work } = user;
+    const userData = { email: email, location: location, age: age, work: work };
+    res.status(200).json({ data: userData });
+  } catch (error) {
+    res.status(500).json({ message: `Error getting data : ${error}` });
   }
 });
 
